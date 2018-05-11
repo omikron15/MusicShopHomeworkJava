75de7e4599a0fd3fe92bbf61781a6562f924d9ee
omikron15 <omikron15@hotmail.co.uk>
2018-05-11 12:20:04 +0100

    Shop class constructor set up

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,25 @@
 package shop;
 
+import interfaces.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import shop.Shop;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void setUp() throws Exception {
+        shop = new Shop("My Shop");
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("My Shop", shop.getName());
+    }
+
+    @Test
+    public void setname() {
+        shop.setName("New Name");
+        assertEquals("New Name", shop.getName());
+    }
 }
