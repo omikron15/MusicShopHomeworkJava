ba8c5096a0651012091edbe5b28c0aaa54f4954e
omikron15 <omikron15@hotmail.co.uk>
2018-05-11 12:46:23 +0100

    Adds stock count, add stock and remove stock functionality to shop class

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -22,4 +22,16 @@ public class Shop {
         this.name = name;
     }
 
+    public int getStockCount(){
+        return this.stock.size();
+    }
+
+    public void addStock(ISell item){
+        stock.add(item);
+    }
+
+    public void removeStock(ISell item){
+        stock.remove(item);
+    }
+
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,3 +1,6 @@
+import item.Accessory;
+import item.instrument.Guitar;
+import item.instrument.Piano;
 import org.junit.Before;
 import org.junit.Test;
 import shop.Shop;
@@ -7,10 +10,16 @@ import static org.junit.Assert.assertEquals;
 public class ShopTest {
 
     Shop shop;
+    Piano piano;
+    Guitar guitar;
+    Accessory accessory;
 
     @Before
     public void setUp() throws Exception {
         shop = new Shop("My Shop");
+        piano = new Piano("Piano 1", 100.00, 200.00, 48);
+        guitar = new Guitar("Guitar 1", 100.00, 200.00, 7);
+        accessory = new Accessory("Drum Sticks", 5.00, 7.50, "Standard Drum Sticks");
     }
 
     @Test
@@ -23,4 +32,26 @@ public class ShopTest {
         shop.setName("New Name");
         assertEquals("New Name", shop.getName());
     }
+
+    @Test
+    public void getStockCount() {
+        assertEquals(0, shop.getStockCount());
+    }
+
+    @Test
+    public void addStock() {
+        shop.addStock(piano);
+        assertEquals(1, shop.getStockCount());
+    }
+
+    @Test
+    public void removeStock() {
+        shop.addStock(piano);
+        shop.addStock(guitar);
+        shop.removeStock(piano);
+        assertEquals(1, shop.getStockCount());
+    }
+
+    // list all instruments
+    //list all accessories
 }
